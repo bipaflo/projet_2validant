390377bb79f398828ec4d14313a60d3fc467e36f
bipaflo <paulbitsindou@gmail.com>
2019-04-11 16:58:16 +0200

    something lignt

--- lib/the_dark_trader.rb
+++ lib/the_dark_trader.rb
@@ -4,38 +4,37 @@ require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
-# Fetch and parse HTML document
+# Analyser HTML document
 page = Nokogiri::HTML(open('https://coinmarketcap.com/all/views/all/'))
 row = page.css('div.table-responsive.compact-name-column table#currencies-all tr')
 
-### Isoler les éléments HTML qui vont bien
+#soler les éléments HTML que l'on veut
 row.each do |row|
   symbol = row.css('td.text-left.col-symbol')
   price = row.css('td.no-wrap.text-right a.price')
 
-### Creer un array des cryptos
+# Créer un array des cryptos
   crypto = []
   symbol.each do |symbol|
    crypto << symbol.text
   end
 
-### Creer un array des prix
+# Créer un array des prix
   value = []
   price.each do |price|
     value << price.text.delete_prefix("$").to_f.round(2)
   end
 
-### Rejoindre les crytpos avec leur prix dans un hash
+# Réunir les crytpos avec leur prix dans un hash
   hash = Hash[crypto.zip(value)]
 
-### Creer un array de hash avec les valeurs
+# Créer un array de hash avec les valeurs
   a = []
   hash.each do
     a << hash
   end
 
-### Imprimer l'array de hash
+# Afficher l'array de hash
   puts a
 
 end
-    
